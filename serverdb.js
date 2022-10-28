034031bfa9610db9b4e5faa128a6221886b02d53
melodytowett <melodytowett99@gmail.com>
2022-10-28 08:49:24 +0300

    added modules

--- serverdb.js
+++ serverdb.js
@@ -1,32 +1,32 @@
-const sequelize= require("sequelize");
-const Sequelize = new sequelize(
-    'library',
-    'root',
-    'meloA@3903.',
-    {
-        host:'localhost',
-        dialect:'mysql'
-    }
-);
-Sequelize.authenticate().then(() => {
-   console.log('Connection has been established successfully.');
-}).catch((error) => {
-   console.error('Unable to connect to the database: ', error);
-});
+// const sequelize= require("sequelize");
+// const Sequelize = new sequelize(
+//     'library',
+//     'root',
+//     'meloA@3903.',
+//     {
+//         host:'localhost',
+//         dialect:'mysql'
+//     }
+// );
+// Sequelize.authenticate().then(() => {
+//    console.log('Connection has been established successfully.');
+// }).catch((error) => {
+//    console.error('Unable to connect to the database: ', error);
+// });
 
-Sequelize.sync().then(() => {
-   console.log('Book table created successfully!');
-   Book.create({
-       title: "Clean Code",
-       author: "Robert Cecil Martin",
-       description:"Teaches on how to write clean code that can be reviewd and understood by other people",
-       release_date: "2021-12-14",
-   }).then(res => {
-       console.log(res)
-   }).catch((error) => {
-       console.error('Failed to create a new record : ', error);
-   });
+// Sequelize.sync().then(() => {
+//    console.log('Book table created successfully!');
+//    Book.create({
+//        title: "Clean Code",
+//        author: "Robert Cecil Martin",
+//        description:"Teaches on how to write clean code that can be reviewd and understood by other people",
+//        release_date: "2021-12-14",
+//    }).then(res => {
+//        console.log(res)
+//    }).catch((error) => {
+//        console.error('Failed to create a new record : ', error);
+//    });
 
-}).catch((error) => {
-   console.error('Unable to create table : ', error);
-});
+// }).catch((error) => {
+//    console.error('Unable to create table : ', error);
+// });
