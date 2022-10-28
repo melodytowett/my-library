edfcb58609ae5fe49dd655ac2460c2ef96050829
melodytowett <melodytowett99@gmail.com>
2022-10-28 09:59:01 +0300

    pushing to book module

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ Sequelize.authenticate().then(() => {
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
-app.listen(7000);
+app.listen(3000);
 
 
 app.get('/',(req,res)=>{
